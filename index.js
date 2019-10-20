80190ba2931b015447d8b40aec32e58c0b494639
Ayodeji Fakunle <ayodejisamuelfakunle@gmail.com>
2019-10-20 20:42:41 +0100

    done with peer to peer chat app, added some new files and made some new changes

--- index.js
+++ index.js
@@ -54,33 +54,57 @@ io.on('connection', function(socket){
     });
 
     socket.on("create room", (starter, to)=>{
-        sessions.find({starter:starter, to:to}).then((session)=>{
+        sessions.findOne({$or: [ {starter:starter._id, to:to._id}, {starter:to._id, to:starter._id}]}).then((session)=>{
+            
             if(!session) {
-                const session = new sessions({starter:starter,to:to});
+                const session = new sessions({starter:starter._id,to:to._id});
                 session.save().then((session)=>{
                     socket.join(session._id);
-                    io.to(starter).emit("create room",to)
+                    console.log("new session", session)
+                    socket.to(session._id).to(to.sessionId).emit("create room", starter, "someone wants to chat with you")
                 }).catch((e)=>{
                     console.log(e)
                 })
-            }else{
+             }else if(session){
+               console.log(session._id)
                 socket.join(session._id);
-               
+                io.to(socket.id).emit("store room", session);
+               socket.broadcast.to(session._id).emit("create room", starter, to,"wants to chat with you click here to join.")
             }
+        }).catch(e=>{
+            console.log(e);
         })
     })
-    socket.on("typing", function(data){
-        socket.broadcast.emit("typing", data);
-    })
-    
-    socket.on("stop typing", function(data){
-        socket.broadcast.emit("stop typing", "");
+
+    socket.on("join room", (starter, to)=>{
+        sessions.findOne({$or: [ {starter:starter._id, to:to._id}, {starter:to._id, to:starter._id}]}).then((session)=>{
+            if(!session) {
+                    console.log("err")
+             }else if(session){
+                socket.join(session._id);
+                console.log("join",session)
+                io.to(socket.id).emit("store room", session);
+               socket.to(session._id).to(to.sessionId).emit("join room", starter, to,"is online")
+            }
+        }).catch(e=>{
+            console.log(e);
+        })
     })
 
-    socket.on("chat message", function(data, id){
-        console.log(usernames[id])
-        io.to(usernames[id]).emit('chat message', data);
+    socket.on("chat message", function(room, starter, message){
+        sessions.findOne({_id:room}).then((session)=>{
+                
+            if(!session) {
+                console.log("err")
+            }else {
+                io.to(session._id).emit("chat message",  starter, message)
+            }
+        }).catch(e=>{
+            console.log(e);
+        })
     })
+
+    
   });
 
   http.listen(3000, function(){
